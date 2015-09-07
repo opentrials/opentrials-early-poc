c6af22aa5a24cec58a4f6de9c79f9749e160e6d2
Levko Kravets <kravets@qarea.com>
2015-09-07 15:52:41 +0300

    #2 Added page for Trial

--- prototype/app.js
+++ prototype/app.js
@@ -15,9 +15,11 @@ nunjucks.configure(views, {
 var controllerIndex = require('./controllers/index');
 var controllerContacts = require('./controllers/contacts');
 var controllerAbout = require('./controllers/about');
+var controllerTrial = require('./controllers/trial');
 
 app.get('/', controllerIndex);
 app.get('/contacts', controllerContacts);
 app.get('/about', controllerAbout);
+app.get('/trial/:id', controllerTrial);
 
 module.exports = app;
