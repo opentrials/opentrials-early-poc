42e89d5083598bcce5fed2176619e0bc7f6ffc8e
Levko Kravets <kravets@qarea.com>
2015-09-07 11:46:50 +0300

    #2 Nunjucks templating framework itegrated

--- prototype/app.js
+++ prototype/app.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var nunjucks  = require('nunjucks');
 var views = path.join(__dirname, '/views');
 var statics = path.join(__dirname, '/public');
 
 app.use(express.static(statics));
 
+nunjucks.configure(views, {
+  autoescape: true,
+  express: app
+});
+
 app.get('/', function(request, response) {
-  response.sendFile(path.join(views, '/base.html'));
+  response.render('base.html', {
+    title: 'Hello'
+  });
 });
 
 module.exports = app;
