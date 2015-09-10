3418239b0f5a0153f18e1fba1ad553a1ce662bfb
Paul Walsh <paulywalsh@gmail.com>
2015-09-10 09:57:48 +0300

    Show how to structure multiple tests

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,6 @@
 var Browser = require('zombie');
 var app = require('../prototype/app');
-// var assert = require('chai').assert;
+var assert = require('chai').assert;
 
 process.env.NODE_ENV = 'test';
 Browser.localhost('127.0.0.1', process.env.PORT || 3000);
@@ -22,5 +22,47 @@ describe('Core', function() {
       done();
     });
   });
+
+  it('is alive', function(done) {
+    browser.visit('/', function() {
+      browser.assert.success();
+      done();
+    });
+  });
+
+  it('Should contain text "Main"', function(done) {
+    browser.visit('/', function() {
+      assert.ok(browser.success);
+      assert.equal(browser.text('h1'), 'Main');
+      done();
+    });
+  });
+
+  it('Should contain text "About"', function(done) {
+    browser.visit('/about', function() {
+      assert.ok(browser.success);
+      assert.equal(browser.text('h1'), 'About');
+      done();
+    });
+  });
+
+  it('Should contain text "Contacts"', function(done) {
+    browser.visit('/contacts', function() {
+      assert.ok(browser.success);
+      assert.equal(browser.text('h1'), 'Contacts');
+      done();
+    });
+  });
+
+  it('Should contain ID of trial', function(done) {
+    var trialId = 1234;
+
+    browser.visit('/trial/' + trialId, function() {
+      assert.ok(browser.success);
+      assert.equal(browser.text('h1'), 'Trial #' + trialId);
+      done();
+    });
+  });
+
 
 });
