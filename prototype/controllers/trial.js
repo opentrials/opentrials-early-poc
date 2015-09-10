9af3d6c7d4ec8382a65312e55db766a9e4710904
Levko Kravets <kravets@qarea.com>
2015-09-10 11:16:36 +0300

    Fixed tests

--- prototype/controllers/trial.js
+++ prototype/controllers/trial.js
@@ -3,7 +3,7 @@ var trialModel = require('../models/trial');
 module.exports = function(request, response) {
   var item = trialModel.findById(request.params.id);
   response.render('trial.html', {
-    title: item.publicTitle + ' (' + item.scientificTitle + ')',
+    title: 'Trial #' + request.params.id + ' ' + item.publicTitle + ' (' + item.scientificTitle + ')',
     item: JSON.stringify(item)
   });
 };
